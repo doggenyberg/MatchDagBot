fab964dc30f0285eac6c55e9f2ed184e77294436
Douglas Nyberg <douglas.nyberg@outlook.com>
2024-08-09 22:37:04 +0200

    added functionality
    finished layout and design for the embedded message.
    
    also added automatic daily notifications to all channels that are set.
    
    added json file to store channels setted

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import http.client
 import urllib.parse
 import json
+import os
 import settings
 from discord.ext import commands
 from datetime import datetime
@@ -10,172 +11,108 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 discord_api = settings.DISCORD_API_SECRET
 football_api = settings.FOOTBALL_API_SECRET
 football_base_url = "v3.football.api-sports.io"
+CHANNELS_FILE = "channels.json"
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!mdb ", intents=intents)
+scheduler = AsyncIOScheduler()
 
-server_channels = {}
+global_rounds = []
 
 
-# Get game info for AIK Stockholm and Hammarby FF
+def load_channels():
+    if os.path.exists(CHANNELS_FILE):
+        with open(CHANNELS_FILE, "r") as file:
+            try:
+                return json.load(file)
+            except json.JSONDecodeError:
+                return {}
+    else:
+        with open(CHANNELS_FILE, "w") as file:
+            json.dump({}, file, indent=4)
+        return {}
+
+
+def save_channels(data):
+    with open(CHANNELS_FILE, "w") as file:
+        json.dump(data, file, indent=1)
+
+
+server_channels = load_channels()
+
+
+# Get game info for AIK and Hammarby
 def fetch_rounds():
-    # conn = http.client.HTTPSConnection("v3.football.api-sports.io")
-    # team_ids = [377, 363]
-    # all_rounds = []
+    conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+    team_ids = [377, 363]
+    all_rounds = []
 
-    # for team_id in team_ids:
-    #     headers = {"x-rapidapi-key": football_api}
-    #     params = urllib.parse.urlencode(
-    #         {
-    #             "team": team_id,
-    #             "next": "1",
-    #             "timezone": "Europe/Stockholm",
-    #         }
-    #     )
-
-    #     try:
-    #         conn.request("GET", f"/fixtures?{params}", headers=headers)
-    #         res = conn.getresponse()
-    #         data = res.read()
-    #         json_data = json.loads(data.decode("utf-8"))
-
-    #         if json_data.get("errors"):
-    #             raise Exception(
-    #                 f"Error fetching data from team ID {team_id}: {json_data['errors']}"
-    #             )
-    #         else:
-    #             all_rounds.extend(json_data.get("response", []))
-
-    #     except Exception as e:
-    #         print(f"Could not retrieve data: {e}")
-    #         raise
-
-    all_rounds = [
-        {
-            "fixture": {
-                "id": 1164216,
-                "referee": None,
+    for team_id in team_ids:
+        headers = {"x-rapidapi-key": football_api}
+        params = urllib.parse.urlencode(
+            {
+                "team": team_id,
+                "next": "1",
                 "timezone": "Europe/Stockholm",
-                "date": "2024-08-11T16:00:00+02:00",
-                "timestamp": 1723384800,
-                "periods": {"first": None, "second": None},
-                "venue": {
-                    "id": 1501,
-                    "name": "Strawberry Arena",
-                    "city": "Solna",
-                },
-                "status": {
-                    "long": "Not Started",
-                    "short": "NS",
-                    "elapsed": None,
-                },
-            },
-            "league": {
-                "id": 113,
-                "name": "Allsvenskan",
-                "country": "Sweden",
-                "logo": "https://media.api-sports.io/football/leagues/113.png",
-                "flag": "https://media.api-sports.io/flags/se.svg",
-                "season": 2024,
-                "round": "Regular Season - 18",
-            },
-            "teams": {
-                "home": {
-                    "id": 377,
-                    "name": "AIK stockholm",
-                    "logo": "https://media.api-sports.io/football/teams/377.png",
-                    "winner": None,
-                },
-                "away": {
-                    "id": 2240,
-                    "name": "Mjallby AIF",
-                    "logo": "https://media.api-sports.io/football/teams/2240.png",
-                    "winner": None,
-                },
-            },
-            "goals": {"home": None, "away": None},
-            "score": {
-                "halftime": {"home": None, "away": None},
-                "fulltime": {"home": None, "away": None},
-                "extratime": {"home": None, "away": None},
-                "penalty": {"home": None, "away": None},
-            },
-        },
-        {
-            "fixture": {
-                "id": 1164216,
-                "referee": None,
-                "timezone": "Europe/Stockholm",
-                "date": "2024-08-11T16:00:00+02:00",
-                "timestamp": 1723384800,
-                "periods": {"first": None, "second": None},
-                "venue": {
-                    "id": 1501,
-                    "name": "Strawberry Arena",
-                    "city": "Solna",
-                },
-                "status": {
-                    "long": "Not Started",
-                    "short": "NS",
-                    "elapsed": None,
-                },
-            },
-            "league": {
-                "id": 113,
-                "name": "Allsvenskan",
-                "country": "Sweden",
-                "logo": "https://media.api-sports.io/football/leagues/113.png",
-                "flag": "https://media.api-sports.io/flags/se.svg",
-                "season": 2024,
-                "round": "Regular Season - 18",
-            },
-            "teams": {
-                "home": {
-                    "id": 123,
-                    "name": "AIK stockholm",
-                    "logo": "https://media.api-sports.io/football/teams/377.png",
-                    "winner": None,
-                },
-                "away": {
-                    "id": 2240,
-                    "name": "Mjallby AIF",
-                    "logo": "https://media.api-sports.io/football/teams/2240.png",
-                    "winner": None,
-                },
-            },
-            "goals": {"home": None, "away": None},
-            "score": {
-                "halftime": {"home": None, "away": None},
-                "fulltime": {"home": None, "away": None},
-                "extratime": {"home": None, "away": None},
-                "penalty": {"home": None, "away": None},
-            },
-        },
-    ]
+            }
+        )
+
+        try:
+            conn.request("GET", f"/fixtures?{params}", headers=headers)
+            res = conn.getresponse()
+            data = res.read()
+            json_data = json.loads(data.decode("utf-8"))
+
+            if json_data.get("errors"):
+                raise Exception(
+                    f"Error fetching data from team ID {team_id}: {json_data['errors']}"
+                )
+            else:
+                all_rounds.extend(json_data.get("response", []))
+
+        except Exception as e:
+            print(f"Could not retrieve data: {e}")
+            raise
 
     return all_rounds
 
 
 def game_info(team_id):
     try:
-        rounds = fetch_rounds()
         next_game = None
-
-        for round in rounds:
+        for round in global_rounds:
             data_ht = round["teams"]["home"]
             data_at = round["teams"]["away"]
-
             if data_ht["id"] == team_id or data_at["id"] == team_id:
                 next_game = round
                 break
 
         if not next_game:
+            print(f"No game found for team")
             return None
 
         return next_game
-    except Exception:
+    except Exception as e:
+        print(f"Could not retrieve data: {e}")
         return None
+
+
+def days_until_game(date_str):
+    try:
+        date_obj = datetime.fromisoformat(date_str)
+        today = datetime.now(date_obj.tzinfo)
+        delta = date_obj - today
+        days_left = delta.total_seconds() / (24 * 3600)
+        return round(days_left)
+
+    except ValueError as e:
+        print(f"ValueError calculating days until match: {e}")
+        return "Okänt antal"
+
+    except Exception as e:
+        print(f"Error calculating days until match: {e}")
+        return "Okänt antal"
 
 
 # Returns team ID from string
@@ -186,6 +123,11 @@ def get_team_id(team_name):
 
 # Formats and returns embed for the bot to send
 def embed_message(game, team_id):
+    def aik_name_check(name):
+        if name == "AIK stockholm":
+            return "AIK"
+        return name
+
     def get_color(team_id):
         if team_id == 377:
             return discord.Colour(int("002D56", 16))
@@ -195,45 +137,36 @@ def embed_message(game, team_id):
 
     def home_or_away():
         if game["teams"]["home"]["id"] == team_id:
-            return f"**{home_team_name}** spelar nästa match mot {away_team_name}"
-        else:
-            return f"**{away_team_name}** spelar nästa match mot {home_team_name}"
-
-    def days_until_match(date_str):
-        try:
-            date_obj = datetime.fromisoformat(date_str)
-            today = datetime.now(date_obj.tzinfo)
-            delta = date_obj - today
-            return delta.days
-
-        except ValueError as e:
-            print(f"ValueError calculating days until match: {e}")
-            return "Okänt antal"
-
-        except Exception as e:
-            print(f"Error calculating days until match: {e}")
-            return "Okänt antal"
+            return f"{home_team_name} spelar nästa match mot {away_team_name}"
+        return f"{away_team_name} spelar nästa match mot {home_team_name}"
 
     home_team_name = game["teams"]["home"]["name"]
     home_team_logo = game["teams"]["home"]["logo"]
     away_team_name = game["teams"]["away"]["name"]
+    away_team_logo = game["teams"]["away"]["logo"]
+    arena = game["fixture"]["venue"]["name"]
+
+    if game["teams"]["home"]["id"] == team_id:
+        team_logo = home_team_logo
+    else:
+        team_logo = away_team_logo
+
+    home_team_name = aik_name_check(home_team_name)
+    away_team_name = aik_name_check(away_team_name)
 
     date_str = game["fixture"]["date"]
     formatted_date = datetime.fromisoformat(date_str).strftime("%Y-%m-%d %H:%M")
-    days_left = days_until_match(date_str)
-
-    arena = game["fixture"]["venue"]["name"]
-    city = game["fixture"]["venue"]["city"]
+    days_left = days_until_game(date_str)
 
     embed = discord.Embed(
-        title=f"**Nästa match**",
-        description=f"{home_or_away()} - **{days_left}** dagar kvar!",
+        title=game["league"]["name"],
+        description=f"{home_or_away()} om **{days_left}** dagar!",
         color=get_color(team_id),
     )
 
-    embed.set_thumbnail(url=home_team_logo)
-    embed.add_field(name="Arena", value=arena, inline=True)
+    embed.set_thumbnail(url=team_logo)
     embed.add_field(name="Datum", value=formatted_date, inline=True)
+    embed.add_field(name="Arena", value=arena, inline=True)
 
     return embed
 
@@ -241,13 +174,16 @@ def embed_message(game, team_id):
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+    scheduler.start()
+    print(f"Scheduler is now started")
 
 
 @bot.command()
 async def set_channel(ctx):
-    server_id = ctx.guild.id
+    server_id = str(ctx.guild.id)
     channel_id = ctx.channel.id
     server_channels[server_id] = channel_id
+    save_channels(server_channels)
     await ctx.send(f'Jag kommer att skicka match-notifieringar i "{ctx.channel.name}"')
 
 
@@ -257,7 +193,7 @@ async def nextgame(ctx, team_name: str):
 
     if not team_id:
         await ctx.send(
-            "Kunde inte hitta laget. Skriv `AIK` eller `Hammarby` efter `!mdb nextgame`"
+            "Kunde inte hitta laget. Skriv `AIK` eller `Hammarby` efter kommandot `!mdb nextgame`"
         )
         return
 
@@ -270,8 +206,37 @@ async def nextgame(ctx, team_name: str):
         else:
             await ctx.send(f"Kunde inte hitta nästa match.")
 
-    except Exception:
+    except Exception as e:
+        print(f"Couldn't send message for !mdb nextgame: {e}")
         await ctx.send(f"Jag behöver vila en stund! Prova igen imorgon.")
 
+
+async def send_game_updates():
+    global server_channels
+    global global_rounds
+
+    server_channels = load_channels()
+    print(f"Loaded server_channels: {server_channels}")
+
+    try:
+        global_rounds = fetch_rounds()
+
+        for server_id, channel_id in server_channels.items():
+            channel = bot.get_channel(int(channel_id))
+            if channel:
+                print(f"Found channel: {channel.name}")
+                for team_name in ["AIK", "Hammarby"]:
+                    team_id = get_team_id(team_name)
+                    next_game = game_info(team_id)
+                    if next_game and days_until_game(next_game["fixture"]["date"]) == 2:
+                        embedded_message = embed_message(next_game, team_id)
+                        await channel.send(embed=embedded_message)
+                        print("Game update was sent successfully")
+                        break
+    except Exception as e:
+        print(f"Couldn't send game update: {e}")
+
+
+scheduler.add_job(send_game_updates, "interval", seconds=10)
 
 bot.run(discord_api)
